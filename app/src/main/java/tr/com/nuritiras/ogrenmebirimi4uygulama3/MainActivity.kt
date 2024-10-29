26b73104d555d20b4ce996e2bdb2c97b3d22d037
Nuri TIRAŞ <47992544+nuritiras@users.noreply.github.com>
2024-10-29 21:09:39 +0300

    Create README.md

--- app/src/main/java/tr/com/nuritiras/ogrenmebirimi4uygulama3/MainActivity.kt
+++ app/src/main/java/tr/com/nuritiras/ogrenmebirimi4uygulama3/MainActivity.kt
@@ -1,6 +1,9 @@
 package tr.com.nuritiras.ogrenmebirimi4uygulama3
 
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,19 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    fun onClickOnayla(view: View) {
+
+        val kullaniciAdi = "fatih"
+        val sifre = "1453"
+
+        val userName = findViewById<EditText>(R.id.editTextUserName).text.toString()
+        val password = findViewById<EditText>(R.id.editTextPassword).text.toString()
+
+        if (kullaniciAdi.equals(userName) && sifre.equals(password))
+            Toast.makeText(this, "Kullanıcı Girişi Başarılı", Toast.LENGTH_LONG).show()
+        else
+            Toast.makeText(this, "Kullanıcı Adı veya Şifresi Hatalı", Toast.LENGTH_LONG).show()
     }
 }
